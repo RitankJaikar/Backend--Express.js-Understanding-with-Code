d1ddd1f5ac5ec4a27ea4730d835e4a9202af77d5
RitankJaikar <ritank.jaikar@gmail.com>
2023-12-20 10:34:29 +0530

    mysql2, faker

--- index.js
+++ index.js
@@ -99,6 +99,8 @@ app.get("/ig/:username",(req,res)=>{//routing- localhost:8080/ig/(variable)
 });
 
 
+
+
 //Handling GET and POST request
 //This is Backend.
 //Go to Frontend and open index.html, then Submit responses.
@@ -109,7 +111,6 @@ app.get("/register",(req,res)=>{
 });
 //GET: used to get some response, data set in query strings (drawbacks): limited, only sting data, visible in URL
 //GET request Data can be accessed using 'req.query', as data here can only be query strings
-
 
 
 //parse data, if data is in urlencoded format (this is request data)
@@ -126,3 +127,89 @@ app.post("/register",(req,res)=>{
 //POST request Data can be accessed by parsing, then using 'req.body' 
 //'req.body' has no default format, we have to define middlewares to parse data
 //if right middleware is not defined, then 'req.body' will be 'undefined'
+
+
+
+
+//inseting random data(using faker) to MySQL database though node(using MySQL2 package)
+const { faker } = require('@faker-js/faker');
+//generates random data i.e. id, name, email, image, password, birthdate, registration date, etc.
+const mysql = require('mysql2');
+//connects/links Node wih MySQL
+
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'DB1',        //database name
+    password: '123'   //MySQL password
+}); //connection established
+
+// let q='SHOW TABLES'; //SQL query
+
+//manually entring data //'?'-> placeholder
+// let q='INSERT INTO user (id, username, email, password) VALUES (?, ?, ?, ?);';   //SQL query
+// let user=["123", "123user","123user@gmil.com","123userPass"];   //to insert new data(row) into user table
+// let user2=["123b", "123userb","123userb@gmil.com","123userPassb"];   //one at a time
+
+//another way to insert new multiple data(rows) into user table
+// let q='INSERT INTO user (id, username, email, password) VALUES ?;'; //single question mark
+// let users=[
+//     ["123c", "123userc","123userc@gmil.com","123userPassc"],
+//     ["123d", "123userd","123userd@gmil.com","123userPassd"],
+//     ["123e", "123usere","123usere@gmil.com","123userPasse"],
+//     ["123f", "123userf","123userf@gmil.com","123userPassf"]
+// ];
+//'user'->'(?,?,?,?)' for single insert, '[users]'-> '?' for one or multiple insert using 'connection.query'
+
+let getRandomUser= () => {
+    return [
+      faker.string.uuid(),
+      faker.internet.userName(),
+      faker.internet.email(),
+      faker.internet.password(),
+    ];    //return array with random data
+}
+let q='INSERT INTO user (id, username, email, password) VALUES ?;';
+let data=[]
+for(let i=1;i<=100;i++){
+    data.push(getRandomUser());
+}   //100 random user data
+
+// simple query
+try{//here 'connection' object has a method/function 'query' that runs SQL query
+    connection.query(q, [data],
+        (err, results, fields)=> {
+            if(err)     throw err;
+            console.log(results); // results contains rows/result of query returned by server
+            // console.log(fields); // fields contains extra meta data about results, if available (not required)
+        }
+    );
+}
+catch(err){ //if error occurs, maybe wrong query syntax, etc.
+    console.log(err);
+}
+/*
+ResultSetHeader {
+    fieldCount: 0,
+    affectedRows: 4,
+    insertId: 0,
+    info: 'Records: 4  Duplicates: 0  Warnings: 0',
+    serverStatus: 2,
+    warningStatus: 0,
+    changedRows: 0
+}*/ //result in terminal for INSERT query
+
+connection.end();   //to close connection with database
+
+
+// let getRandomUser= () => {
+//   return {
+//     id: faker.string.uuid(),
+//     username: faker.internet.userName(),
+//     email: faker.internet.email(),
+//     password: faker.internet.password(),
+//   };    //return object with random data
+// }
+// console.log(getRandomUser());
